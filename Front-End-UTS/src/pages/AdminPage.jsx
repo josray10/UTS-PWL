37b696b63fb76abadc5313021465e21caab67120
jossySaragih <61966720+josray10@users.noreply.github.com>
2023-10-28 22:19:41 +0700

    uts pwl ra

---
+++ Front-End-UTS/src/pages/AdminPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AdminPage = () => {
+  return (
+    <div>
+      <h1>Halaman Admin</h1>
+      {/* Konten halaman admin */}
+    </div>
+  );
+};
+
+export default AdminPage;
---
+++ Front-End-UTS/src/pages/ProductForm.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const ProductForm = ({ onSubmit }) => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Validasi input di sini
+    onSubmit({ name, description, price });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Price"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default ProductForm;
